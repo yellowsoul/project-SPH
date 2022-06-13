2de566bd62f5047822eba3c9041b574613b02c61
yellowsoul <517878555@qq.com>
2022-06-13 18:43:36 +0800

    P34  mockjs模拟数据+获取Banner轮播图的数据

--- app/src/mock/mockServe.js
+++ app/src/mock/mockServe.js
@@ -8,3 +8,4 @@ import floor from './floor.json';
 // mock数据:第一个参数请求地址：  第二个参数：请求数据
 Mock.mock("/mock/banner", {code:200, data:banner}); //模拟首页大的轮播图的数据
 Mock.mock("/mock/floor", {code:200, data:floor}); 
+
