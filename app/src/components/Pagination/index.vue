6d94aea21a2c81bfb4ed92e64d1dcf9ca612e285
yellowsoul <517878555@qq.com>
2022-06-16 23:23:47 +0800

    P57 055-尚硅谷-尚品汇-分页器起始与结束数字计算（60%）

--- app/src/components/Pagination/index.vue
+++ app/src/components/Pagination/index.vue
@@ -11,16 +11,61 @@
     <button>7</button>
 
     <button>···</button>
-    <button>9</button>
+    <button>{{totalPage}}</button>
     <button>下一页</button>
 
-    <button style="margin-left: 30px">共 60 条</button>
+    <button style="margin-left: 30px">共 {{total}} 条</button>
+    <h1>{{startNumAndEndNum}}---{{pageNo}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: "Pagination",
+  props:["pageNo","pageSize","total","continues"],
+  // 计算属性
+  computed:{
+    // 总共多少页
+    totalPage(){
+      return Math.ceil(this.total / this.pageSize);
+    },
+    // 计算出连续的页码的起始数字与结束数字[连续页码的数字：至少是5]
+    startNumAndEndNum(){
+      const { continues, pageNo, totalPage } = this;
+      // 先定义两个变量存储起始数字与结束数字
+      let start = 0, end = 0;
+      // 连续页码数5【就是至少5页】，如果出现不正常的现象【就是不够5页】
+      // 不正常现象【总页数没有连续页码多】
+      if(continues > totalPage ){
+        start = 1;
+        end = totalPage;
+      }else{
+        // 正常现象【连续页码5，但是你的总页数一定是大于5的】
+        // 起始数字
+        start = pageNo - parseInt(continues/2);
+        // 结束数字
+        end = pageNo + parseInt(continues/2);
+
+        // 比如当前是第8页  连续页码 5
+        // 6 7 【8】 9 10
+        // 比如当前页是第8页  连续页码 7 
+        // 5 6 7 【8】 9 10 11
+
+        // 把出现不正常的现象【start数字出现0|负数】纠正
+        if(start < 1){
+          start = 1;
+          end = continues;
+        }
+
+        // 把出现不正常的现象【end数字大于总页码】纠正
+        if(end > totalPage){
+          end = totalPage;
+          start = totalPage - continues + 1;
+        }
+      }
+      return {start, end}
+    }
+  }
 };
 </script>
 
--- app/src/pages/Search/index.vue
+++ app/src/pages/Search/index.vue
@@ -121,8 +121,8 @@
               </li>
             </ul>
           </div>
-          <!-- 分页器 -->
-          <Pagination/>
+          <!-- 分页器:测试分页器阶段，这里数据将来需要替换的 -->
+          <Pagination :pageNo="31" :pageSize="3" :total="91" :continues="5"/>
         </div>
       </div>
     </div>
