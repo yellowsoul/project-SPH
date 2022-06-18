1dfa1613ef36182bca9cf4e3bf4fea02ed489dbe
yellowsoul <517878555@qq.com>
2022-06-18 23:08:00 +0800

    P68 067-尚硅谷-尚品汇-放大镜结束(黄龙优化)

--- app/src/pages/Detail/ImageList/ImageList.vue
+++ app/src/pages/Detail/ImageList/ImageList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="swiper-container">
+  <div class="swiper-container" ref="cur">
     <div class="swiper-wrapper">
       <div class="swiper-slide" v-for="(slide, index) in skuImageList" :key="slide.id">
-        <img :src="slide.imgUrl">
+        <img :src="slide.imgUrl" :class="{active:currentIndex==index}" @click="changeCurrentIndex(index)">
       </div>
     </div>
     <div class="swiper-button-next"></div>
@@ -15,7 +15,39 @@
   import Swiper from 'swiper'
   export default {
     name: "ImageList",
-    props:['skuImageList']
+    data(){
+      return{
+        currentIndex:0
+      }
+    },
+    props:['skuImageList'],
+    watch:{
+      // 监听数据：可以保证数据一定ok，但是不能保证v-for遍历结构是否完事。
+      skuImageList(newValue,oldValue){
+        this.$nextTick(() => {
+          new Swiper(this.$refs.cur, {
+            loop: false, // 循环模式选项
+
+            // 如果需要前进后退按钮
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+            // 显示几个图片设置
+            slidesPerView:3,
+            slidesPerGroup:1
+          });
+        })
+      }
+    },
+    methods:{
+      changeCurrentIndex(index){
+        // 修改响应式数据
+        this.currentIndex = index;
+        // 通知兄弟组件，当前的索引值为几
+        this.$bus.$emit('getIndex', this.currentIndex);
+      }
+    }
   }
 </script>
 
@@ -44,10 +76,10 @@
           padding: 1px;
         }
 
-        &:hover {
-          border: 2px solid #f60;
-          padding: 1px;
-        }
+        // &:hover {
+        //   border: 2px solid #f60;
+        //   padding: 1px;
+        // }
       }
     }
 
--- app/src/pages/Detail/Zoom/Zoom.vue
+++ app/src/pages/Detail/Zoom/Zoom.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="spec-preview">
+  <div class="spec-preview" ref="small">
     <img :src="imgObj.imgUrl" />
-    <div class="event"></div>
+    <div class="event" @mousemove="handler"></div>
     <div class="big">
-      <img :src="imgObj.imgUrl" />
+      <img :src="imgObj.imgUrl" ref="bigimg"/>
     </div>
-    <div class="mask"></div>
+    <!-- 遮罩层 -->
+    <div class="mask" ref="mask"></div>
   </div>
 </template>
 
@@ -13,9 +14,56 @@
   export default {
     name: "Zoom",
     props:['skuImageList'],
+    data(){
+      return {
+        currentIndex:0
+      }
+    },
     computed:{
       imgObj(){
-        return this.skuImageList[0] || {}
+        return this.skuImageList[this.currentIndex] || {}
+      }
+    },
+    mounted(){
+      // 全局事件总线：获取兄弟组件传递过来的索引值
+      this.$bus.$on('getIndex',(index) => {
+        // 修改当前响应式数据
+        this.currentIndex = index;
+      })
+    },
+    methods:{
+      // 
+      handler(event){
+        let mask = this.$refs.mask;
+        let maskW = mask.offsetWidth;
+        let maskH = mask.offsetHeight;
+        let left = event.offsetX - maskW / 2;
+        let top = event.offsetY - maskH / 2;
+       
+        // 约束范围
+        let samll = this.$refs.small
+        let samllW = samll.offsetWidth;
+        let samllH = samll.offsetHeight;
+        let maxX = samllW - maskW;
+        let maxY = samllH - maskH;
+
+        if(left<=0)left = 0;
+        if(left>=maxX)left = maxX;
+        if(top<=0)top = 0;
+        if(top>maxY) top = maxY;
+
+        // 修改元素的left|top属性值
+        mask.style.left = left + 'px'
+        mask.style.top = top + 'px'
+
+        // 计算大图位置
+        let big = this.$refs.bigimg;
+        let bigW = big.offsetWidth;
+        let rate = bigW / samllW;
+
+        big.style.left = -left * rate + 'px'
+        big.style.top = -top * rate + 'px'
+
       }
     }
   }
